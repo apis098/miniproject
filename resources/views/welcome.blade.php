c8b04c8db7ee32ed16ad255ac53fb0d25ed50aa4
Ghofur102 <muhamadabdulghofur84@gmail.com>
2023-07-31 14:44:26 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>HummaCook</title>
+        <title>Mini Project - HUMMACOOK</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
