ff8830aa5cf39e2a0f4575d4c766805cc80ca240
frdynsyhz <138851667+frdynsyhz@users.noreply.github.com>
2023-08-01 21:20:39 +0700

    update navbar
    memperbaiki navbar

--- resources/views/template/about.blade.php
+++ resources/views/template/about.blade.php
@@ -53,23 +53,23 @@
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav  mx-auto ">
-                <li class="nav-item active">
-                  <a class="nav-link" href="{{route('home')}}">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="{{route('menu')}}">Menu</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="{{route('about')}}">About</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="{{route('book')}}">Book Table</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('login')}}">Login</a>
+                <ul class="navbar-nav  mx-auto ">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="{{route('home')}}">Home <span class="sr-only">(current)</span></a>
                   </li>
-              </ul>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{route('menu')}}">Resep</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{route('SpecialDays.index')}}">Hari Khusus</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="{{route('book')}}">Panduan</a>
+                  </li>
+                  {{-- <li class="nav-item">
+                    <a class="nav-link" href="{{route('login')}}">Login</a>
+                  </li> --}}
+                </ul>
             <div class="user_option">
               <a href="" class="user_link">
                 <i class="fa fa-user" aria-hidden="true"></i>
@@ -133,8 +133,8 @@
                   <i class="fa fa-search" aria-hidden="true"></i>
                 </button>
               </form>
-              <a href="" class="order_online">
-                Order Online
+              <a href="{{route('login')}}" class="order_online">
+                Login
               </a>
             </div>
           </div>
