c28a070be3cf3392a73d3cd1f555fdee5900057c
frdynsyhz <138851667+frdynsyhz@users.noreply.github.com>
2023-08-03 14:31:33 +0700

    update from ferdi
    memperbaiki tampilan admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,9 +1,7 @@
-{{-- @extends('layouts.navbar') --}}
-@include('layouts.navbar')
-@include('layouts.header')
-@include('layouts.sidebar')
+@extends('layouts.navbar')
+
 @section('konten')
 <h4>Selamat Datang <b>{{Auth::user()->name}}</b>, Anda Login sebagai <b>{{Auth::user()->role}}</b>.</h4>
 @endsection
-@include('layouts.footer')
+
 
