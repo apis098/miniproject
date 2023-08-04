04f8a4cddc6cc9be4ca6adadc3303953cc95bad1
Apis.nr <122329006+apis098@users.noreply.github.com>
2023-08-04 13:41:56 +0700

    Update fitur konsultasi koki
    update apis.

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('konten')
 <div class="container">
-    <img src="{{ asset('images/welcome2.jpg') }}" class="img-fluid" style="margin-top:-10%" alt="Gambar Contoh">
+    <img src="{{ asset('images/welcome2.jpg') }}" class="img-fluid" style="margin-top:-10%" alt="welcome">
 </div>
 {{-- <h4>Selamat Datang <b>{{Auth::user()->name}}</b>, Anda Login sebagai <b>{{Auth::user()->role}}</b>.</h4> --}}
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\basic_tips_controller;
+use App\Http\Controllers\complaintController;
 use App\Http\Controllers\kategori_bahan_controller;
 use App\Http\Controllers\kategori_tipsdasar_controller;
 use App\Http\Controllers\KokiController;
@@ -56,7 +57,7 @@ Route::get('specialday', function () {
     return view('admin.specialday');
 })->name('specialday');
 
-
+Route::post('/keluhan-store', [complaintController::class, 'store'])->name('ComplaintUser.store');
 // Login Register & logout
 
 Route::get('login', [LoginController::class, 'login'])->name('login');
@@ -95,7 +96,6 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
         Route::resource('edit-tentang', AboutController::class);
     });
 });
-
 
 // role koki
 Route::get('koki/index', [KokiController::class, 'index'])->name('koki.index')->middleware('auth', 'role:koki');
