4f49a4fca9ced58cbd96805924a75e43208f3d6d
agglnxz <140498082+agglnxz@users.noreply.github.com>
2023-08-06 21:32:01 +0700

    mengganti crud drputar dapur menjadi modal

--- resources/views/admin/seputar-dapur/seputardapur.blade.php
+++ resources/views/admin/seputar-dapur/seputardapur.blade.php
@@ -49,7 +49,7 @@
                     <label for="isi" class="col-sm-2 col-form-label">input isi </label>
                     <div class="col-sm-10">
                         <textarea class="form-control" name="isi" @error('isi')
-                            @enderror id="tiny"></textarea>
+                            @enderror id="isi" rows="5"></textarea>
                         @error('isi')
                             <div class="alert alert-danger mt-2">
                                 {{ $message }}
@@ -100,7 +100,8 @@
                     <form action="{{ route('seputar_dapur.destroy', $data->id) }}" method="POST" class="d-inline-block">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="btn btn-outline-danger btn-sm rounded-5"
+
+                      <button type="submit" class="btn btn-outline-danger btn-sm rounded-5"
                             data-mdb-ripple-color="dark"
                             onclick="return confirm('Are you sure you want to delete this data?')"><i
                                 class="fa-solid fa-trash-can"></i></button>
@@ -110,5 +111,102 @@
         @endforeach
     </tbody>
 </table>
-@endsection
 
+@foreach ($seputar_dapur as $item)
+@if ($item->id != '')
+    {{-- modal edit --}}
+    <div class="modal fade" id="exampleModal{{ $item->id }}" tabindex="-1" role="dialog"
+        aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Form Edit Data</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('seputar_dapur.update', $item->id) }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        <div class="mb-3 row">
+                            <label for="foto" class="col-sm-2 col-form-label">Input Foto</label>
+                            <div class="col-sm-10">
+                                <input type="file" class="form-control" @error('foto')
+                                @enderror name="foto" id="foto" accept="image/*">
+                                <img src="{{ asset('storage/public/seputardapur/'.$item->foto) }}" class="rounded" style="width: 150px">
+                                @error('foto')
+                                <div class="alert alert-danger mt-2">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="judul" class="col-sm-2 col-form-label">input judul</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" value="{{ $data->judul }}" name="judul" id="judul">
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="kategori" class="col-sm-2 col-form-label">input kategori</label>
+                            <div class="col-sm-10">
+                                <select name="kategori_id" @error('kategori_id')
+                                @enderror id="kategori" class="form-control">
+                                    <option value="">-- Pilih Kategori --</option>
+                                    @foreach ($kategori_seputardapur as $data)
+                                    <option
+                                    {{ $data->id ? 'selected' : ''}} value="{{ $data->id }}">{{ $data->nama_kategori }}
+                                     </option>
+
+                                    @endforeach
+                                </select>
+                                @error('kategori_id')
+                  <div class="alert alert-danger mt-2">
+                    {{ $message }}
+                  </div>
+                  @enderror
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <label for="isi" class="col-sm-2 col-form-label">input isi </label>
+                            <div class="col-sm-10">
+                                <textarea class="form-control" rows="5" name="isi" @error('isi')
+                                    @enderror id="isi">{{ $item->isi }}</textarea>
+                                @error('isi')
+                                    <div class="alert alert-danger mt-2">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                        </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit"
+                        class="btn btn-primary  rounded-5 mb-1 zoom-effects d-flex align-items-center"
+                        data-mdb-ripple-color="dark">
+                        <i class="fa-regular fa-floppy-disk me-1"></i>
+                        Submit
+                    </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endif
+@endforeach
+{{-- end modal edit --}}
+@endsection
+<script>
+    $(document).ready(function() {
+        $('#search').on('input', function() {
+            var value = $(this).val().toLowerCase();
+            $('#table tbody tr').filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        });
+    });
+</script>
