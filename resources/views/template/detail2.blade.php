059cce7e9b44da3525a7cdb2317b0cd2035227f5
Apis.nr <122329006+apis098@users.noreply.github.com>
2023-08-07 13:59:50 +0700

    update detail.blade.php
    form apis

--- resources/views/template/detail2.blade.php
+++ resources/views/template/detail2.blade.php
@@ -9,7 +9,8 @@
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Bootstrap icons-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"
+        
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/styles.css" rel="stylesheet" />
     </head>
@@ -23,10 +24,10 @@
         <section class="py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="row gx-4 gx-lg-5 align-items-center">
-                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://dummyimage.com/600x700/dee2e6/6c757d.jpg" alt="..." /></div>
-                    <div class="col-md-6">
+                    <div class="col-md-4"><img class="card-img-top mb-5 mb-md-0" src="{{ asset('images/message.jpg') }}" alt="..." /></div>
+                    <div class="col-md-8">
                         {{-- <div class="small mb-1">SKU: BST-498</div> --}}
-                        <h1 class="display-5 fw-bolder">Judul Makanan</h1>
+                        <h2 class="display-5 fw-bolder">Judul Makanan</h2>
                         <div class="fs-5 mb-5">
                             {{-- <span class="text-decoration-line-through">$45.00</span> --}}
                             {{-- <span>$40.00</span> --}}
@@ -38,4 +39,5 @@
         </section>
             </div>
     </body>
+    
 </html>
