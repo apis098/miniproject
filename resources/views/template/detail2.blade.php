732d0721338a65c771bee21ece350c328e19ea19
Andre-humma <141007307+Andre-humma@users.noreply.github.com>
2023-08-07 18:40:37 +0700

    update icon detail
    update icon detail

--- resources/views/template/detail2.blade.php
+++ resources/views/template/detail2.blade.php
@@ -8,7 +8,8 @@
     <meta name="author" content="" />
     <title>HummaCook</title>
     <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" type="">
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.png') }}" />
     <!-- Bootstrap icons-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" <!--
