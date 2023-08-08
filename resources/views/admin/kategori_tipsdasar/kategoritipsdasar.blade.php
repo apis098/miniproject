759ee044741c130326f8cbab44d34aeb09c223f5
Andre-humma <141007307+Andre-humma@users.noreply.github.com>
2023-08-08 20:26:59 +0700

    update tampilan
    update tampilan

--- resources/views/admin/kategori_tipsdasar/kategoritipsdasar.blade.php
+++ resources/views/admin/kategori_tipsdasar/kategoritipsdasar.blade.php
@@ -1,24 +1,58 @@
 @extends('layouts.navbar')
 @section('konten')
+@push('style')
+@powerGridstyles
+@endpush
+<style>
+    .table-rounded {
+        border-collapse: separate;
+        border-radius: 10px;
+        border-color: black;
+
+    }
+
+    .table-rounded thead th:first-child {
+        border-top-left-radius: 10px;
+    }
+
+    .table-rounded thead th:last-child {
+        border-top-right-radius: 10px;
+    }
+
+    .table-rounded tbody tr:last-child td:first-child {
+        border-bottom-left-radius: 10px;
+    }
+
+    .table-rounded tbody tr:last-child td:last-child {
+        border-bottom-right-radius: 10px;
+    }
+
+    .btn-group-vertical {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .zoom-effects:hover {
+        transform: scale(0.97);
+    }
+</style>
         {{-- heading --}}
-    <div class="card container my-5">
-        <div class="card-header bg-dark text-white">
-            <h4 class="text-center"> Kategori Tips Dasar</h4>
-        </div>
-        {{--  tanbah data --}}
-        <div class="card-body">
-            <form action="/admin/kategori-tipsdasar" method="post">
-                {{ csrf_field() }}
-                <div class="mb-3">
-                    <label for="nama_kategori" class="form-label">Kategori Tips Dasar</label>
-                    <input type="text" class="form-control" id="nama_kategori" name="nama_kategori" required>
-                </div>
-                <button type="submit" class="btn btn-secondary">Simpan</button>
+     <div class="mb-2 mt-1 mb-md-1">
+        <label for="name" class="mb-1 ms-2 text-secondary">Tambah Data:</label>
+        <div>
+            <form action="{{ route('kategori-tipsdasar.store') }}" method="POST" class="d-flex align-items-center">
+                @csrf
+                <input type="text" name="nama_kategori" class="form-control ms-1 mb-1 me-2 rounded-3" id="nama_kategori" aria-describedby="emailHelp" placeholder="Inputkan Kategori....">
+                <input type="hidden" value="-" class="form-control" id="description" name="description" placeholder="masukkan Deskripsi...">
+                <button type="submit" class="btn btn-primary btn-sm rounded-5 mb-1 zoom-effects d-flex align-items-center" data-mdb-ripple-color="dark">
+                    <i class="fa-regular fa-floppy-disk me-1"></i>
+                    Submit
+                </button>
             </form>
         </div>
-        <div class="card-footer">
-            <table class="table table-striped table-bordered">
-                <thead class="table-dark">
+    </div>
+    <table class="table table-striped table-rounded" id="table">
+        <thead class="bg-secondary text-light">
                     <tr>
                         <th scope="col">No</th>
                         <th scope="col">Nama Kategori</th>
@@ -35,13 +69,17 @@
                             <td>{{ $item->created_at }}</td>
                             <td>{{ $item->updated_at }}</td>
                             <td>
-                                <div class="d-flex">
+                                {{-- <div class="d-flex"> --}}
                                 <!-- Button trigger modal -->
-                                <button type="button" class="btn btn-warning" data-bs-toggle="modal"
-                                    data-bs-target="#exampleModal{{ $item->id }}">
-                                    Edit
+                                    <button type="button" class="btn btn-outline-success btn-sm rounded-5 edit-btn" data-bs-toggle="modal"
+                                       data-bs-target="#exampleModal{{ $item->id }}">
+                                    <i class="fa-solid fa-pen-clip"></i>
                                 </button>
-
+                                <form action="{{ route('kategori-tipsdasar.destroy', $item->id) }}" method="post" class="d-inline-block">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-outline-danger btn-sm rounded-5" data-mdb-ripple-color="dark" onclick="return confirm('Apakah Anda Yakin Menghapus Kategori Seputar Dapur? Ini?')"><i class="fa-solid fa-trash-can"></i></button>
+                                </form>
                                 <!-- Modal -->
                                 <div class="modal fade" id="exampleModal{{ $item->id }}" tabindex="-1" aria-labelledby="exampleModalLabel"
                                     aria-hidden="true">
@@ -70,12 +108,7 @@
                                     </div>
                                 </div>
 
-                                <form action="/admin/kategori-tipsdasar/{{ $item->id }}" method="post">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger"
-                                        onclick="return confirm('Yakin mau menghapus data kategori tips dasar?')">Hapus</button>
-                                </form>
+
                                 </div>
                             </td>
                         </tr>
