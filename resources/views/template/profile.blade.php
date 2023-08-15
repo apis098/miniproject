72aceba912ce55189f33bab144afbc5c0724365f
Andre-humma <141007307+Andre-humma@users.noreply.github.com>
2023-08-15 18:25:45 +0700

    memperbarui tampilan profil
    memperbarui tampilan profil

--- resources/views/template/profile.blade.php
+++ resources/views/template/profile.blade.php
@@ -5,13 +5,10 @@
             <div class="col-lg-4 mb-5">
                 <div class="card my-5">
                     <div class="card-body text-center">
-                        <img src="{{ asset('Vector.jpg') }}" width="50%" alt="">
-                        
-                        <p class="mt-4"
-                            style="width: 100%; height: 100%; color: black; font-size: 24px; font-family: Poppins; font-weight: 600; word-wrap: break-word">
+                        <img src="{{ asset('Vector.jpg') }}" width="50%" alt="" data-bs-toggle="modal" data-bs-target="#mymodal">
+                        <p class="mt-4" style="width: 100%; height: 100%; color: black; font-size: 24px; font-family: Poppins; font-weight: 600; word-wrap: break-word">
                             Boerak smith
-                            <span
-                                style="width: 100%; height: 100%; color: rgba(0, 0, 0, 0.50); font-size: 16px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Burakcook@gmail.com</span>
+                            <span style="width: 100%; height: 100%; color: rgba(0, 0, 0, 0.50); font-size: 16px; font-family: Poppins; font-weight: 400; word-wrap: break-word">Burakcook@gmail.com</span>
                         </p>
                         <button style="border-radius: 15px;" class="btn btn-light border border-dark mb-3">
                             <span style="font-weight: 600">Buat Resep</span>
@@ -19,6 +16,63 @@
                     </div>
                 </div>
             </div>
+            {{-- awal modal --}}
+            <div class="modal fade" data-bs-backdrop="static" id="mymodal" aria-hidden="true" tabindex="-1">
+                <div class="modal-dialog modal-dialog-centered profile-modal">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5">My Profile</h1>
+                    </div>
+                    <div class="modal-body">
+                      <form action="#" method="POST" enctype="multipart/form-data">
+                          @csrf
+                          @method('put')
+                          <div class="profile d-flex justify-content-center">
+                              <img src="{{ asset('vector.jpg') }}" class="rounded-circle profile-image">
+                              <label for="fileInputA" class="change-profile-button d-flex justify-content-center" id="chooseFileButtonA">
+                                <i class="fa-sharp fa-solid fa-image text-primary"></i>
+                              </label>
+                              <input type="file" id="fileInputA" name="fileInputA" style="display:none" accept=".jpg,.png,.pdf">
+                            </div>
+                          </div>
+
+                      <div class="modal-footer">
+                        <button class="btn btn-danger" class="btn-close" data-bs-dismiss="modal" type="button">Batal</button>
+                        <button class="btn btn-primary" type="submit" id="saveProfileButton">Simpan</button>
+                      </div>
+                    </form>
+                    </div>
+                    <script>
+                      document.getElementById('chooseFileButtonA').addEventListener('click', function() {
+                          document.getElementById('fileInputA').click();
+                      });
+
+                      document.getElementById('fileInputA').addEventListener('change', function() {
+                          var selectedFile = this.files[0];
+                          // Lakukan sesuatu dengan file yang dipilih, misalnya mengunggahnya ke server
+                          console.log('Selected file:', selectedFile);
+                      });
+
+                      $(document).ready(function() {
+                          $('.change-profile-button').on('click', function(e) {
+                              e.preventDefault();
+                          });
+                      });
+
+                      document.getElementById("fileInputA").addEventListener("change", function(event) {
+                          var input = event.target;
+                          if (input.files && input.files[0]) {
+                              var reader = new FileReader();
+                              reader.onload = function(e) {
+                                  document.getElementById("profile-image").setAttribute("src", e.target.result);
+                              };
+                              reader.readAsDataURL(input.files[0]);
+                          }
+                      });
+                  </script>
+                  </div>
+                </div>
+                {{-- akhir modal --}}
             <div class="col-lg-8 mt-5">
                 <div class="row mt-5">
                     <div class="col-lg-4">
