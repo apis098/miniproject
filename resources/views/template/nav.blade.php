e99969f14155d9c345cd8b4f08121b5266df7f05
frdynsyhz <138851667+frdynsyhz@users.noreply.github.com>
2023-08-15 14:11:21 +0700

    menambahkan form resepp

--- resources/views/template/nav.blade.php
+++ resources/views/template/nav.blade.php
@@ -235,7 +235,6 @@
                                 </li>
                                 <li class="nav-item {{ request()->is('search-account') ? 'active' : '' }}" style="font-size: 15px">
                                     <a class="nav-link" href="{{ url('/search-account') }}"><b>Cari Akun</b></a>
-
                                 </li>
 
                                 <li class="nav-item {{ request()->is('about') ? 'active' : '' }} me-2" style="font-size: 15px">
@@ -247,7 +246,7 @@
                             </ul>
                             <div class="user_option" style="margin-left: 180px;">
 
-                            
+
                                 @if (Auth::check())
                                     {{-- <a href="{{ route('actionlogout') }}" class="btn btn-outline-light rounded-5"
                                         style="border-radius: 12px;"><b class="ms-2 me-2">Logout</b></a> --}}
