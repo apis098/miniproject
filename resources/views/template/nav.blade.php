7cd75e8f156039ac743ab7d08cc3ccf4086e635f
frdynsyhz <138851667+frdynsyhz@users.noreply.github.com>
2023-08-17 19:34:31 +0700

    menambahkan view resep berdasrkan bahan

--- resources/views/template/nav.blade.php
+++ resources/views/template/nav.blade.php
@@ -253,14 +253,14 @@
                                             <a data-toggle="dropdown" class="text-light" href="#">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><path fill="currentColor" d="M4 19v-2h2v-7q0-2.075 1.25-3.688T10.5 4.2v-.7q0-.625.438-1.063T12 2q.625 0 1.063.438T13.5 3.5v.7q2 .5 3.25 2.113T18 10v7h2v2H4Zm8-7.5ZM12 22q-.825 0-1.413-.588T10 20h4q0 .825-.588 1.413T12 22Zm-4-5h8v-7q0-1.65-1.175-2.825T12 6q-1.65 0-2.825 1.175T8 10v7Z"/></svg>
                                             </a>
-                                          
+
                                             <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right mt-1" style="width: 350px; border-radius:13px; margin-right:-105%;" >
                                                 @foreach($notification as $row)
                                                 @if($row->sender->id != auth()->user()->id)
-                                                    
+
                                                     <div class="dropdown-divider"></div>
                                                             <div class="input-group">
-                                                              
+
                                                                 <a href="#">
                                                                     <img class="ms-2 mb-1 rounded-circle"
                                                                     src="{{ asset('images/client1.jpg') }}"
@@ -284,12 +284,12 @@
                                                                         alt="profile image" style="max-width:10px"></a>
                                                                 @endif
                                                             </div>
-                                                        
+
                                                 @endif
                                                 @endforeach
                                                 <div class="dropdown-divider"></div>
                                             </div>
-                                            
+
                                         </div>
 
                                         {{-- dropdown profile & logout --}}
@@ -336,7 +336,7 @@
     </div>
     @yield('content')
     <!-- footer section -->
-    <footer class="footer_section" style="background-color: #F7941E; ">
+    <footer class="footer_section radius-atas" style="background-color: #F7941E; ">
         <div class="container">
             <div class="row">
                 <div class="col-md-4 footer-col">
