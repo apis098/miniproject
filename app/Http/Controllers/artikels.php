d1954a3e69269f263407a5580b534f4b9594c9a4
Ghofur102 <muhamadabdulghofur84@gmail.com>
2023-08-21 08:56:29 +0700

    membuat tampilan artikel resep

--- app/Http/Controllers/artikels.php
+++ app/Http/Controllers/artikels.php
@@ -7,12 +7,23 @@ use App\Models\reseps;
 use App\Models\seputar_dapur;
 use App\Models\special_days;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\notifications;
 
 class artikels extends Controller
 {
     public function artikel_resep(string $id, string $judul)
     {
+        $userLogin = Auth::user();
+        $notification = [];
+        $unreadNotificationCount=[];
+        if ($userLogin) {
+            $notification = notifications::where('user_id', auth()->user()->id)
+                ->orderBy('created_at', 'desc') // Urutkan notifikasi berdasarkan created_at terbaru
+                ->paginate(10); // Paginasi notifikasi dengan 10 item per halaman
+                $unreadNotificationCount = notifications::where('user_id',auth()->user()->id)->where('status', 'belum')->count();
+        }
         $show_resep = reseps::find($id);
-        return view('template.artikel', compact('show_resep'));
+        return view('template.artikel', compact('show_resep', 'notification','unreadNotificationCount','userLogin'));
     }
 }
--- resources/views/template/artikel.blade.php
+++ resources/views/template/artikel.blade.php
@@ -1,298 +1,121 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <link rel="shortcut icon" href="images/favicon.png" type="">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
-    </script>
-    <title> HummaCook </title>
-
-    <!-- bootstrap core css -->
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
-
-    <!--owl slider stylesheet -->
-    <link rel="stylesheet" type="text/css"
-        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
-    <!-- nice select  -->
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css"
-        integrity="sha512-CruCP+TD3yXzlvvijET8wV5WxxEh5H8P4cmz0RFbKK6FlZ2sYl3AEsKlLPHbniXKSrDdFewhbmBK5skbdsASbQ=="
-        crossorigin="anonymous" />
-    <!-- font awesome style -->
-    <link href="css/font-awesome.min.css" rel="stylesheet" />
-
-    <!-- Custom styles for this template -->
-    <link href="css/style.css" rel="stylesheet" />
-    <!-- responsive style -->
-    <link href="css/responsive.css" rel="stylesheet" />
-    <link href="{{ asset('css/style.css') }} " rel="stylesheet">
-    <link href="{{ asset('style.css') }} " rel="stylesheet">
-
-    <!-- Responsive CSS -->
-    <link href="{{ asset('css/responsive/responsive.css') }}" rel="stylesheet">
-    <style>
-        .nav-link.active {
-            background-color: #f39c12;
-            color: #fff;
-        }
-
-        .nav-link {
-            white-space: nowrap;
-        }
-
-
-        .dropdown {
-            position: relative;
-            display: inline-block;
-        }
-
-        .dropdown .dropbtn {
-
-            border: none;
-            outline: none;
-            color: white;
-            background-color: inherit;
-            margin: 0;
-            padding: 14px 16px;
-        }
-
-        .dropdown:hover .dropdown-content {
-            display: block;
-        }
-
-        .dropdown-content {
-            display: none;
-            position: absolute;
-            background-color: #1d1919;
-            min-width: 160px;
-            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-            z-index: 1;
-        }
-
-        .dropdown-content a {
-
-            color: black;
-            padding: 12px 16px;
-            text-decoration: none;
-            display: block;
-
-        }
-
-        .dropdown-content a:hover {
-            background-color: #f1f1f146;
-        }
-
-        .dropdown:hover .dropdown-content {
-            display: block;
-        }
-
-        .login {
-            display: inline-block;
-            padding: 6px 22px;
-            background-color: #ffbe33;
-            color: #ffffff;
-            border-radius: 45px;
-            -webkit-transition: all 0.3s;
-            transition: all 0.3s;
-            border: none;
-        }
-
-
-        .dotted {
-            border: 2px dotted #aaa;
-            text-align: center;
-            padding: 10px;
-            width: 300px;
-            height: auto;
-            border-radius: 20px;
-        }
-
-        .black-border-button {
-
-
-            border: 1px solid black;
-            padding: 10px 15px;
-            font-size: 14px;
-            cursor: pointer;
-        }
-    </style>
-
-
-
-
-
-</head>
-
-<body>
-
-
-    <!-- ****** Header Area Start ****** -->
-    <header class="header_area">
-        <div class="container">
-            <div class="row">
-                <!-- Logo Area Start -->
-                <div class="col-12">
-                    <div class="logo_area text-center">
-
-                        <a href="{{ route('home') }}" class="yummy-logo">HummaCook Blog</a>
+@extends('template.nav')
+@section('content')
+    <div class="row">
+        <div class="col-lg-10">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <img style="border-radius: 9999px; border: 0.50px black solid"
+                            src="{{ asset('storage/' . $show_resep->foto_resep) }}" />
                     </div>
-                </div>
-            </div>
-            <hr>
-
-
-        </div>
-    </header>
-    <!-- ****** Header Area End ****** -->
-
-    <!-- ****** Breadcumb Area Start ****** -->
-
-
-
-    <!-- ****** Breadcumb Area End ****** -->
-
-    <!-- ****** Single Blog Area Start ****** -->
-    <section class="single_blog_area section_padding_80">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-12">
-                    <!-- Single Post -->
-                    <div class="col-10 col-sm-11">
-                        <div class="single-post">
-                            <!-- Post Thumb -->
-                            <div class="post-thumb" style="display: flex; justify-content: center;">
-                                <img src="{{ asset('storage/' . $show_resep->foto_resep) }}" width="25%"
-                                    alt="{{$show_resep->foto_resep}}" class="">
-                            </div>
-                            <h1>{{ $show_resep->nama_resep }}</h1>
-                            <!-- Post Content -->
-                            <div class="post-content">
-
-                                <div class="post-meta d-flex">
-                                    <div class="post-author-date-area d-flex">
-                                        <!-- Post Author -->
-                                        <div class="post-author">
-                                            <a href="#">Oleh <span
-                                                    class="text-info">{{ $show_resep->User->name }}</span></a>
-                                        </div>
-                                        <!-- Post Date -->
-                                        <div class="post-date">
-
-                                            <a href="#">{{ $show_resep->created_at }}</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <a href="#">
-
-                                <h2 class="post-headline"> </h2>
-                            </a>
-                            <p>
-                                {!! $show_resep->deskripsi_resep !!}
-                            </p>
-
-                            <blockquote class="yummy-blockquote mt-30 mb-30">
-                                <h5 class="mb-30">
-                                    Bahan - Bahan <br>
-                                    @foreach ($show_resep->bahan as $item_bahan)
-                                        <button
-                                            class="btn btn-light border rounded p-2 m-2">{{ $item_bahan->nama_bahan }} {{$item_bahan->takaran_bahan}}</button>
-                                        <br>
-                                    @endforeach
-                                </h5>
-                            </blockquote>
-                            <h4>Langkah - Langkah</h4>
-                            @foreach ($show_resep->langkah as $l)
-                                <div 
-                                    class="card-body border rounded p-2 m-2">
-                                    <img src="{{asset('storage/'.$l->foto_langkah)}}" alt="">
-                                    {{ $l->deskripsi_langkah }}
-                                </div>
-                                <br>
-                            @endforeach
+                    <div class="col-lg-9 my-5">
+                        <div>
+                            <h4>{{ $show_resep->nama_resep }}</h4>
+                            <span>Oleh {{ $show_resep->User->name }}</span>
                         </div>
                     </div>
-
-                    <!-- Tags Area -->
-
-                    {{-- <div class="tags-area">
-                                <a href="#">Multipurpose</a>
-                                <a href="#">Design</a>
-                                <a href="#">Ideas</a>
-
-                            </div> --}}
-
                 </div>
             </div>
         </div>
+        <div class="col-lg-2">
+            <button type="submit" class="btn btn-light my-5" style="border: 1px solid black">Sukai</button>
         </div>
-        </div>
-    </section>
-    <!-- ****** Single Blog Area End ****** -->
-
-    <!-- ****** Footer Menu Area Start ****** -->
-    <footer class="footer_area">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="footer-content">
-                        <!-- Logo Area Start -->
-                        <div class="footer-logo-area text-center">
-
-                            <a href="{{ route('home') }}" class="yummy-logo">HummaCook Blog</a>
-                        </div>
-                        <!-- Menu Area Start -->
-
-
+    </div>
+    <div class="row mt-2 container mx-5">
+        <div class="col-lg-4">
+            <h4>Durasi</h4>
+            <div class="card p-3" style="border-radius: 30px; border: 0.50px black solid">
+                <div class="row my-1 container">
+                    <div class="col-10 ">
+                        <span class="ms-3 my-auto"
+                            style="color: black; font-size: 28px; font-family: Poppins; font-weight: 400; word-wrap: break-word">
+                            {{ $show_resep->lama_memasak }}
+                        </span> <br>
+                    </div>
+                    <div class="col-2 my-3">
+                        <i class="fa-regular fa-clock fa-2xl"></i>
                     </div>
                 </div>
             </div>
         </div>
-
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <!-- Copywrite Text -->
-                    <div class="copy_right_text text-center">
-
-                        <p>Copyright @2023 All rights reserved | This template is made with <i class="fa fa-heart-o"
-                                aria-hidden="true"></i> by <a href="{{ route('home') }}" target="_blank">HummaCook</a>
-                        </p>
+        <div class="col-lg-4">
+            <h4>Pengeluaran</h4>
+            <div class="card p-3" style="border-radius: 30px; border: 0.50px black solid">
+                <div class="row my-1 container">
+                    <div class="col-10">
+                        <span class="ms-3"
+                            style="color: black; font-size: 28px; font-family: Poppins; font-weight: 400; word-wrap: break-word">
+                            RP{{ number_format($show_resep->pengeluaran_memasak, 2, ',', '.') }}
+                        </span> <br>
+                    </div>
+                    <div class="col-2 my-3">
+                        <i class="fa-solid fa-money-bill fa-2xl"></i>
                     </div>
                 </div>
             </div>
         </div>
-    </footer>
-    <!-- ****** Footer Menu Area End ****** -->
-
-    <!-- Jquery-2.2.4 js -->
-    <script src="js/jquery/jquery-2.2.4.min.js"></script>
-    <!-- Popper js -->
-    <script src="js/bootstrap/popper.min.js"></script>
-    <!-- Bootstrap-4 js -->
-    <script src="js/bootstrap/bootstrap.min.js"></script>
-    <!-- All Plugins JS -->
-    <script src="js/others/plugins.js"></script>
-    <!-- Active JS -->
-    <script src="js/active.js"></script>
-</body>
-
-</html>
+        <div class="col-lg-4">
+            <h4>Porsi</h4>
+            <div class="card p-3" style="border-radius: 30px; border: 0.50px black solid">
+                <div class="row my-1 container">
+                    <div class="col-10">
+                        <span class="ms-3"
+                            style="color: black; font-size: 28px; font-family: Poppins; font-weight: 400; word-wrap: break-word">
+                            {{ $show_resep->porsi_orang }} Orang
+                        </span> <br>
+                    </div>
+                    <div class="col-2 my-3">
+                        <i class="fa-solid fa-plate-wheat fa-2xl"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <ul class="nav nav-tabs container mt-5" id="myTab" role="tablist">
+        <li class="nav-item mr-5" role="presentation">
+            <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane"
+                type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">
+                <h5>Home</h5>
+            </button>
+        </li>
+        <li class="nav-item mr-5" role="presentation">
+            <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button"
+                role="tab" aria-controls="profile-tab-pane" aria-selected="false">
+                <h5>Bahan - Bahan</h5>
+            </button>
+        </li>
+        <li class="nav-item mr-5" role="presentation">
+            <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" type="button"
+                role="tab" aria-controls="contact-tab-pane" aria-selected="false">
+                <h5>Langkah - Langkah</h5>
+            </button>
+        </li>
+    </ul>
+    <div class="tab-content container" id="myTabContent">
+        <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+            <div class="card-body mb-5">
+                {{ $show_resep->deskripsi_resep }}
+            </div>
+        </div>
+        <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
+            <h4 class="mt-2">Bahan</h4>
+            <div class="row">
+                @foreach ($show_resep->bahan as $b)
+                    <div class="col-lg-4 mx-1 rounded p-2" style="border: 1px solid black;">
+                        <h6>{{ $b->nama_bahan }}</h6> 
+                        <span>{{ $b->takaran_bahan }}</span>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+        <div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">
+            @foreach ($show_resep->langkah as $n => $l)
+                <div class="card-body">
+                    {{$n+=1}}
+                    <img class="mr-4" src="{{ asset('storage/' . $l->foto_langkah) }}" alt="{{ $l->foto_langkah }}" style="border-radius: 15px;">
+                    {{ $l->deskripsi_langkah }}
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
