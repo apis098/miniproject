998edade0d4ac7a968daf41d84b7e4721ac1f6ca
agglnxz <140498082+agglnxz@users.noreply.github.com>
2023-08-22 18:07:09 +0700

    menambah  fungsi show password

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -46,7 +46,7 @@
             overflow: hidden;
         }
 
-  
+
         .circle {
           background: #ffffff;
         border-radius: 50%;
@@ -375,12 +375,41 @@
 
                                 <!-- Password input -->
                                 <div class="form-outline mb-3">
-                                    <input type="password" name="password" class=" username form-control rounded-4"
+                                    <input type="password" name="password" id="pass2" class=" username form-control rounded-4"
                                         placeholder="Kata Kunci..." required="">
+                                        <div class="input-group-append">
+
+                                          <!-- kita pasang onclick untuk merubah icon buka/tutup mata setiap diklik  -->
+                                          <span id="mybutton2" onclick="change('pass2','mybutton2')" class="input-group-text">
+
+                                              <!-- icon mata bawaan bootstrap  -->
+                                              <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-fill" fill="currentColor"
+                                                  xmlns="http://www.w3.org/2000/svg">
+                                                  <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                                                  <path fill-rule="evenodd"
+                                                      d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+                                              </svg>
+                                          </span>
+                                      </div>
+
                                 </div>
                                 <div class="form-outline mb-3">
-                                    <input type="password" name="copassword" id="copassword" class=" username form-control rounded-4"
+                                    <input type="password" name="copassword" id="pass" class=" username form-control rounded-4"
                                         placeholder="Konfirmasi Kata Sandi..." required="">
+                                        <div class="input-group-append">
+
+                                          <!-- kita pasang onclick untuk merubah icon buka/tutup mata setiap diklik  -->
+                                          <span id="mybutton" onclick="change('pass','mybutton')" class="input-group-text">
+
+                                              <!-- icon mata bawaan bootstrap  -->
+                                              <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-fill" fill="currentColor"
+                                                  xmlns="http://www.w3.org/2000/svg">
+                                                  <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                                                  <path fill-rule="evenodd"
+                                                      d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+                                              </svg>
+                                          </span>
+                                      </div>
                                 </div>
                                 <br>
                                 <!-- Submit button -->
@@ -420,7 +449,34 @@
                 svgPlaceholder.style.display = "block"; // Show the SVG placeholder
             }
         });
+
+          // membuat fungsi change
+          function change(inputId, buttonId) {
+
+            var input = document.getElementById(inputId);
+            var x = input.type;
+            var button = document.getElementById(buttonId);
+
+
+              if (input.type === 'password') {
+                input.type = 'text';
+                button.innerHTML = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-slash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M10.79 12.912l-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z"/>
+                                    <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708l-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829z"/>
+                                    <path fill-rule="evenodd" d="M13.646 14.354l-12-12 .708-.708 12 12-.708.708z"/>
+                                    </svg>`;
+              }
+              else {
+                input.type = 'password';
+                button.innerHTML = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+                                    <path fill-rule="evenodd" d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+                                    </svg>`;
+              }
+          }
+
     </script>
+
 </body>
 </html>
 
