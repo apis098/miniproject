f17da6e6a97b5d48e71e18806d36bd8c0d4dd173
frdynsyhz <138851667+frdynsyhz@users.noreply.github.com>
2023-08-28 14:42:56 +0700

    update waktu di komentar

--- resources/views/replies/detail.blade.php
+++ resources/views/replies/detail.blade.php
@@ -1,7 +1,6 @@
 @extends('template.nav')
 @section('content')
     <style>
-
         .card {
 
             border: none;
@@ -20,7 +19,7 @@
             display: inline-block;
         }
 
-  
+
 
         .user-img {
 
@@ -92,6 +91,7 @@
         .intro-2 {
             font-size: 13px
         }
+
         .modal-body {
             padding: 20px;
             /* Tambahkan padding untuk menciptakan jarak di sekitar elemen */
@@ -101,6 +101,7 @@
             margin-right: 20px;
             /* Tambahkan margin di sebelah kanan gambar untuk menciptakan jarak */
         }
+
         .as {
             color: black;
             font-size: 20px;
@@ -177,15 +178,43 @@
                             {{ session('error') }}
                         </div>
                     @endif
+                    <style>
+                        .post-content {
+  max-height: 100px; /* Atur tinggi maksimum konten yang ditampilkan */
+  overflow: hidden; /* Sembunyikan teks yang berlebihan */
+}
+
+.read-more-button {
+  background-color: #007BFF;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-top: 10px;
+   /* Sembunyikan tombol secara default */
+}
+
+.post.open .post-content {
+  max-height: none; /* Tampilkan seluruh teks saat tombol ditekan */
+}
+
+.post.open .read-more-button {
+  /* Sembunyikan tombol saat teks ditampilkan secara penuh */
+}
+
+
+                      </style>
+
+
                     @foreach ($replies as $row)
                         <div class="card p-3">
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="user d-flex flex-row align-items-center">
                                     @if ($row->user->foto)
-                                        <img src="{{ asset('storage/' . $row->user->foto) }}" width="30"
+                                        <img src="{{ asset('storage/' . $row->user->foto) }}" width="30" height="30"
                                             class="user-img rounded-circle mr-2">
                                     @else
-                                        <img src="{{ asset('images/default.jpg') }}" width="30"
+                                        <img src="{{ asset('images/default.jpg') }}" width="30" height="30"
                                             class="user-img rounded-circle mr-2">
                                     @endif
                                     @if ($row->user->role == 'admin')
@@ -195,50 +224,65 @@
                                                     width="15" height="15" viewBox="0 0 24 24">
                                                     <path fill="currentColor"
                                                         d="m10.6 16.6l7.05-7.05l-1.4-1.4l-5.65 5.65l-2.85-2.85l-1.4 1.4l4.25 4.25ZM12 22q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Zm0-2q3.35 0 5.675-2.325T20 12q0-3.35-2.325-5.675T12 4Q8.65 4 6.325 6.325T4 12q0 3.35 2.325 5.675T12 20Zm0-8Z" />
-                                                </svg></small><small
-                                                class="font-weight-bold">{{ $row->reply }}</small></span>
-                                    @else
-                                        <span><small
-                                                class="font-weight-semibold text-primary ms-2 me-2"><b>{{ $row->user->name }}</b></small><small
-                                                class="font-weight-bold">{{ $row->reply }}</small></span>
-                                    @endif
+                                                </svg></small>
+                                            <br>
+                                            <div class="w-auto p-2" style="">
+                                                <small class="font-weight-bold">{{ $row->reply }}</small>
+                                        </span>
                                 </div>
-                                @if ($repliesCount > 0)
-                                    <small>{{ $row->created_at->diffForHumans(['short' => false]) }}</small>
-                                @endif
+                            @else
+                                <span><small
+                                        class="font-weight-semibold text-primary ms-2 me-2"><b>{{ $row->user->name }}</b></small>
+                                    <br>
+                                    <div class="w-auto p-2 " style="">
+                                        <small class="font-weight-bold" >{{ $row->reply }}</small>
+                                        {{-- @if (strlen($row->reply) > 500) --}}
+                            {{-- <button class="read-more-button">Baca Selengkapnya</button> --}}
+                        {{-- @endif --}}
+                                </span>
                             </div>
-                            <div class="action d-flex justify-content-between mt-2 align-items-center">
-
-                                <div class="reply px-7 me-2">
-                                    <small id="like-count-{{$row->id}}"> {{ $row->likes }}</small>
-                                </div>
-
-                                <div class="icons align-items-center input-group">
-
-                                    <form action="{{ route('Replies.like', $row->id) }}" method="POST" class="like-form">
-                                        @csrf
-                                        @if ($userLogin && $row->likes()->where('user_id', $userLogin->id)->exists())
-                                            <button type="submit" class="yuhu me-2 text-warning btn-sm rounded-5 like-button ">
-                                                <i class="fa-solid fa-thumbs-up"></i>
-                                            </button>
-                                        @else
-                                            <button type="submit" class="yuhu me-2 text-dark btn-sm rounded-5 like-button">
-                                                <i class="fa-regular fa-thumbs-up"></i>
-                                            </button>
-                                        @endif
-                                    </form>
-                                    <button type="button" data-toggle="modal" data-target="#Modal{{ $row->id }}"
-                                        class="yuhu text-danger btn-sm rounded-5 "><i
-                                            class="fa-solid fa-triangle-exclamation me-2"></i></button>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
+                    @endif
                 </div>
-                @foreach($replies as $row)
-                    @if($row->id != null)
+                @if ($repliesCount > 0)
+                    <div class="w-75 p-3">
+                        <small class="float-end">{{ $row->created_at->diffForHumans(['short' => false]) }}</small>
+                    </div>
+                @endif
+            </div>
+            <div class="action d-flex justify-content-between mt-2 align-items-center">
+
+                <div class="reply px-7 me-2">
+                    <small id="like-count-{{ $row->id }}"> {{ $row->likes }}</small>
+                </div>
+
+                <div class="icons align-items-center input-group">
+
+                    <form action="{{ route('Replies.like', $row->id) }}" method="POST" class="like-form">
+                        @csrf
+                        @if (
+                            $userLogin &&
+                                $row->likes()->where('user_id', $userLogin->id)->exists())
+                            <button type="submit" class="yuhu me-2 text-warning btn-sm rounded-5 like-button ">
+                                <i class="fa-solid fa-thumbs-up"></i>
+                            </button>
+                        @else
+                            <button type="submit" class="yuhu me-2 text-dark btn-sm rounded-5 like-button">
+                                <i class="fa-regular fa-thumbs-up"></i>
+                            </button>
+                        @endif
+                    </form>
+                    <button type="button" data-toggle="modal" data-target="#Modal{{ $row->id }}"
+                        class="yuhu text-danger btn-sm rounded-5 "><i
+                            class="fa-solid fa-triangle-exclamation me-2"></i></button>
+                </div>
+            </div>
+        </div>
+        @endforeach
+        </div>
+        @foreach ($replies as $row)
+            @if ($row->id != null)
                 <!-- Modal -->
-                <div class="modal fade" id="Modal{{ $row->id }}"  tabindex="-1" role="dialog"
+                <div class="modal fade" id="Modal{{ $row->id }}" tabindex="-1" role="dialog"
                     aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                     <div class="modal-dialog modal-dialog-centered" role="document">
                         <div class="modal-content">
@@ -248,17 +292,20 @@
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
-                            <form action="{{ route('report.reply',$row->id) }}" method="POST">
+                            <form action="{{ route('report.reply', $row->id) }}" method="POST">
                                 @csrf
                                 <div class="modal-body d-flex align-items-center" style="background-color: #ffffff;">
                                     <!-- Tambahkan kelas "align-items-center" -->
                                     @if ($row->user->foto)
-                                        <img class="rounded-circle" src="{{ asset('storage/' . $row->user->foto) }}" width="106px" height="104px"
-                                            style="border-radius: 50%; max-width:110px; border:0.05rem solid rgb(185, 180, 180);" alt="">
+                                        <img class="rounded-circle" src="{{ asset('storage/' . $row->user->foto) }}"
+                                            width="106px" height="104px"
+                                            style="border-radius: 50%; max-width:110px; border:0.05rem solid rgb(185, 180, 180);"
+                                            alt="">
                                         <textarea class="form-control" name="description" rows="5" placeholder="Alasan"></textarea>
                                     @else
                                         <img src="{{ asset('images/default.jpg') }}" width="106px" height="104px"
-                                            style="border-radius: 50%; max-width:110px; border:0.05rem solid rgb(185, 180, 180);" alt="">
+                                            style="border-radius: 50%; max-width:110px; border:0.05rem solid rgb(185, 180, 180);"
+                                            alt="">
                                         <textarea class="form-control rounded-5" name="description" rows="5" placeholder="Alasan..."></textarea>
                                     @endif
                                 </div>
@@ -271,12 +318,53 @@
                         </div>
                     </div>
                 </div>
-                    @endif
-                @endforeach
-            </div>
+            @endif
+        @endforeach
+        </div>
         </div>
     </section>
     </div>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            const readMoreButtons = document.querySelectorAll(".read-more-button");
+
+            readMoreButtons.forEach((readMoreButton) => {
+                readMoreButton.addEventListener("click", function () {
+                    const post = this.closest(".post"); // Temukan kontainer komentar terdekat
+                    const postContent = post.querySelector(".post-content");
+
+                    post.classList.toggle("open");
+
+                    if (post.classList.contains("open")) {
+                        this.textContent = "Sembunyikan";
+                        postContent.style.maxHeight = "none"; // Tampilkan seluruh teks saat tombol ditekan
+                    } else {
+                        this.textContent = "Baca Selengkapnya";
+                        postContent.style.maxHeight = "100px"; // Ganti dengan tinggi maksimum yang Anda inginkan
+                    }
+                });
+            });
+
+            const posts = document.querySelectorAll(".post");
+
+            posts.forEach((post) => {
+                const postContent = post.querySelector(".post-content");
+                const readMoreButton = post.querySelector(".read-more-button");
+                const maxLength = 500; // Atur panjang maksimum yang Anda inginkan
+
+                // Periksa panjang teks komentar dan tambahkan tombol "Baca Selengkapnya" hanya jika melebihi panjang maksimum
+                if (postContent.textContent.length <= maxLength) {
+                    readMoreButton.style.display = "none";
+                } else {
+                    postContent.style.maxHeight = "100px"; // Sembunyikan teks yang berlebihan secara default
+                }
+            });
+        });
+    </script>
+
+
+
     <script>
         document.addEventListener("DOMContentLoaded", function() {
             const likeForms = document.querySelectorAll(".like-form");
@@ -301,13 +389,15 @@
                         if (responseData.liked) {
                             button.classList.remove('text-dark');
                             button.classList.add('text-warning');
-                            icon.setAttribute('class', 'fa-solid fa-thumbs-up'); 
-                            document.getElementById("like-count-" + responseData.reply_id).textContent = responseData.likes;
+                            icon.setAttribute('class', 'fa-solid fa-thumbs-up');
+                            document.getElementById("like-count-" + responseData.reply_id)
+                                .textContent = responseData.likes;
                         } else {
                             button.classList.remove('text-warning');
                             button.classList.add('text-dark');
-                            icon.setAttribute('class', 'fa-regular fa-thumbs-up'); 
-                            document.getElementById("like-count-" + responseData.reply_id).textContent = responseData.likes;
+                            icon.setAttribute('class', 'fa-regular fa-thumbs-up');
+                            document.getElementById("like-count-" + responseData.reply_id)
+                                .textContent = responseData.likes;
                         }
                     }
                 });
