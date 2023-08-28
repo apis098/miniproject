cc7fd98f39cb77ad49c4c0d5246f41b707c69fd6
Ghofur102 <muhamadabdulghofur84@gmail.com>
2023-08-28 19:30:01 +0700

    perbaiki form tambah resep

--- resources/views/koki/resep.blade.php
+++ resources/views/koki/resep.blade.php
@@ -28,16 +28,16 @@
                         </div>
                     </div>
                     <div class="row"
-                        style="box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 15px; border: 0.50px rgb(142, 136, 136) solid; height: 40x;">
+                        style="box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 15px; border: 0.50px rgb(142, 136, 136) solid; height: 40px;">
                         <button type="button" onclick="klik()" class="col-4"
-                            style="border: 0.50px rgb(255, 148, 47) solid;background: #F7941E; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 15px; padding: 9px 12px; right: 2px; width: 20px">
-                            <div
-                                style="color: #EAEAEA; font-size: 14px; font-family: Poppins; font-weight: 600; word-wrap: break-word">
-                                Pilih File</div>
+                            style="border: 0.50px rgb(255, 148, 47) solid;background: #F7941E; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 15px; padding: 9px 12px; right: 2px; width: 100px;height: 40px;">
+                            <p
+                                style="color: #EAEAEA; font-size: 14px; font-family: Poppins; font-weight: 600; word-wrap: break-word;">
+                                Pilih File</p>
                             <input name="foto_resep" class="form-control my-auto mx-1" style="display: none;" type="file"
                                 id="formFile">
                         </button>
-                        <div class="col-8 my-auto" id="infos"
+                        <div class="col-7 my-auto" id="infos"
                             style="color: black; font-size: 14px; font-family: Poppins; font-weight: 600; word-wrap: break-word">
                             Tidak ada file terpilih</div>
                     </div>
@@ -273,7 +273,7 @@
                                     <input type="radio" class="btn-check" name="hari_khusus"
                                         id="success-outlined{{$int}}" autocomplete="off" checked>
                                     <label class="btn btn-outline-warning" 
-                                    style="border-radius: 15px;box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); color: black;border:none;"
+                                    style="border-radius: 15px;box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);border:none;"
                                     for="success-outlined{{$int}}">{{ $d->nama }}</label>
                                 </div>
                             @endforeach
