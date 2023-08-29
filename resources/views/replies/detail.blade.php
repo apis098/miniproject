b31003bcc694ce012d9634987c4589df7724c83b
Andre-humma <141007307+Andre-humma@users.noreply.github.com>
2023-08-29 21:40:52 +0700

    menambahkan modal

--- resources/views/replies/detail.blade.php
+++ resources/views/replies/detail.blade.php
@@ -94,6 +94,16 @@
                         </div>
                     </div>
                     <p>{{ $data->description }}</p>
+                    <div style="text-align: right;">
+                        <button type="submit" class="btn btn-warning zoom-effects text-light btn-sm rounded-circle p-2"
+                            style="background-color:#F7941E;" data-toggle="modal"
+                            data-target="#exampleModalCenter">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 20 20">
+                                <path fill="currentColor"
+                                    d="M3.5 2.75a.75.75 0 0 0-1.5 0v14.5a.75.75 0 0 0 1.5 0v-4.392l1.657-.348a6.449 6.449 0 0 1 4.271.572a7.948 7.948 0 0 0 5.965.524l2.078-.64A.75.75 0 0 0 18 12.25v-8.5a.75.75 0 0 0-.904-.734l-2.38.501a7.25 7.25 0 0 1-4.186-.363l-.502-.2a8.75 8.75 0 0 0-5.053-.439l-1.475.31V2.75Z" />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
 
             </div>
@@ -133,6 +143,41 @@
                             {{ session('error') }}
                         </div>
                     @endif
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+                    aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <form action="{{ route('Report.store') }}" method="POST">
+                                @csrf
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalCenterTitle">Report User</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="d-flex align-items-center">
+                                        @if(auth()->user()->foto)
+                                            <img src="{{ asset('storage/'.auth()->user()->foto) }}" width="106px" height="104px"
+                                                style="border-radius: 50%" alt="">
+                                        @else
+                                            <img src="{{ asset('images/default.jpg') }}" width="106px" height="104px"
+                                                style="border-radius: 50%" alt="">
+                                        @endif
+                                        <textarea class="form-control" name="description" rows="5"
+                                            placeholder="Alasan">{{ old('description') }}</textarea>
+                                        <input type="hidden" name="profile_id" value="{{ auth()->user()->id }}">
+                                        <input type="hidden" name="user_id" value="{{ auth()->user()->id }}">
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="submit" class="btn btn-light text-light" style="border-radius: 15px; background-color:#F7941E;"><b class="ms-2 me-2">Laporkan</b></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
                     <style>
                         .post-content {
   max-height: 100px; /* Atur tinggi maksimum konten yang ditampilkan */
