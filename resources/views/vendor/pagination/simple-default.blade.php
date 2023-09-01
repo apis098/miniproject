3b77581baa28530f9999379fcc49787bdb228c22
Ghofur102 <muhamadabdulghofur84@gmail.com>
2023-09-01 09:37:33 +0700

    up

--- resources/views/vendor/pagination/simple-default.blade.php
+++ resources/views/vendor/pagination/simple-default.blade.php
@@ -4,11 +4,7 @@
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
             @else
-                <li><button class="btn btn-warning mx-5" 
-                    style="background-color: #f39c12; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 15px;"
-                    >
-                    <a href="{{ $paginator->previousPageUrl() }}" style="color: white;" rel="prev">Selanjutnya</a>
-                </button></li>
+              
             @endif
 
             {{-- Next Page Link --}}
@@ -20,6 +16,11 @@
                     <a href="{{ $paginator->nextPageUrl() }} " style="color: white;" rel="next">Selanjutnya</a>
                 </button></li>
             @else
+            <li><button class="btn btn-warning mx-5" 
+                style="background-color: #f39c12; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 15px;"
+                >
+                <a href="{{ $paginator->previousPageUrl() }}" style="color: white;" rel="prev">Selanjutnya</a>
+            </button></li>
             @endif
         </ul>
     </nav>
