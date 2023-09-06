f7142a19e27e5d639a7e361abacda1c1c52830cd
Ghofur102 <muhamadabdulghofur84@gmail.com>
2023-09-06 13:08:40 +0700

    Update artikel.blade.php

--- resources/views/template/artikel.blade.php
+++ resources/views/template/artikel.blade.php
@@ -226,7 +226,6 @@
                                 <path fill="currentColor"
                                     d="M15 1H9v2h6V1zm-4 13h2V8h-2v6zm8.03-6.61l1.42-1.42c-.43-.51-.9-.99-1.41-1.41l-1.42 1.42A8.962 8.962 0 0 0 12 4c-4.97 0-9 4.03-9 9s4.02 9 9 9a8.994 8.994 0 0 0 7.03-14.61zM12 20c-3.87 0-7-3.13-7-7s3.13-7 7-7s7 3.13 7 7s-3.13 7-7 7z" />
                             </svg>
-
                         </div>
                     </div>
                 </div>
@@ -458,11 +457,11 @@
                             </form>
                     </div>
                 @else
-                    <form action="/komentar-resep/{{ Auth::user()->id }}/{{ $show_resep->id }}" method="POST">
+                    <form>
                         @csrf
                         <div class="input-group">
                             <input type="text" id="reply" name="komentar" width="500px" maxlength="255"
-                                {{ $userLog === 1 ? 'disabled' : '' }} class="form-control rounded-3 me-5"
+                                class="form-control rounded-3 me-5"
                                 placeholder="{{ $userLog === 1 ? 'Tambah Komentar' : 'Tambah Komentar' }}">
                             {{-- <button class="btn btn-primary rounded-2 me-2"><i class="fa-solid fa-face-laugh-beam"></i></button> --}}
                             <button type="button" onclick="harusLogin()"
