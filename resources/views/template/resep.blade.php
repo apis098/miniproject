72af297321cd560f68d7fcb7354dce9ce0316c19
Ghofur102 <muhamadabdulghofur84@gmail.com>
2023-09-06 08:52:48 +0700

    memberikan collapse di filter resep

--- resources/views/template/resep.blade.php
+++ resources/views/template/resep.blade.php
@@ -247,7 +247,7 @@
                         @endif
                         <div class="mb-3">
                             <label for="ingredients" class="form-label">Nama Bahan</label>
-                            <select name="ingredients[]" aria-placeholder="Masukkan nama bahan" multiple="multiple" id="ingredients"
+                            <select style="border: 1px solid black" name="ingredients[]" aria-placeholder="Masukkan nama bahan" multiple="multiple" id="ingredients"
                                 class="cari form-control">
                                 <option value="" disabled>Masukkan Nama Bahan</option>
 
@@ -317,16 +317,33 @@
                         </div>
                         <div class="mb-3">
                             <label for="hari" class="form-label">Hari spesial</label>
-                            <div class="row">
+                            <div class="row" style="width: 100%;">
                                 @foreach ($special_day as $nums => $d)
-                                    <div class="col-lg-3 mb-3">
-                                        <input type="hidden" id="input_pilih_hari{{ $nums }}"
-                                            value="{{ $d->nama }}">
-                                        <button id="pilih_hari{{ $nums }}"
-                                            onclick="pilih_hari({{ $nums }})" class="btn btn-light"
-                                            type="button"
-                                            style="width: 100%;border: 1px solid black; border-radius: 10px;font-size: 10px;">{{ $d->nama }}</button>
-                                    </div>
+                                @if ($nums <= 3)
+                                <div class="col-lg-3 mb-3" style="width:100%;">
+                                    <input type="hidden" id="input_pilih_hari{{ $nums }}"
+                                        value="{{ $d->nama }}">
+                                    <button id="pilih_hari{{ $nums }}" 
+                                        onclick="pilih_hari({{ $nums }})" class="btn btn-light"
+                                        type="button"
+                                        style="border: 1px solid black; border-radius: 10px;font-size: 10px;">{{ $d->nama }}</button>
+                                </div>   
+                                @else
+                                  <div class="collapse" id="collapseExample{{$nums}}">
+                                        <div class="col-lg-3 mb-3" style="margin-left: -10px;">
+                                            <input type="hidden" id="input_pilih_hari{{ $nums }}"
+                                                value="{{ $d->nama }}">
+                                            <button id="pilih_hari{{ $nums }}"
+                                                onclick="pilih_hari({{ $nums }})" class="btn btn-light"
+                                                type="button"
+                                                style="width: 100%;border: 1px solid black; border-radius: 10px;font-size: 10px;">{{ $d->nama }}</button>
+                                        </div>  
+                                  </div>   
+                                  <a class="text-center text-dark" data-bs-toggle="collapse" href="#collapseExample{{$nums}}" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                    Lihat Lebih Banyak <i class="fa-solid fa-chevron-down"></i>
+                                  </a>               
+                                @endif
+                                    
                                 @endforeach
                             </div>
                         </div>
@@ -334,14 +351,30 @@
                             <label for="jenis_makanan" class="form-label">Jenis Makanan</label>
                             <div class="row">
                                 @foreach ($categories_foods_all as $num => $f)
-                                    <div class="col-lg-3 mb-3">
-                                        <input id="input_jenis_makanan{{ $num }}" type="hidden"
-                                            value="{{ $f->nama_makanan }}">
-                                        <button id="pilih_jenis_makanan{{ $num }}"
-                                            onclick="pilih_jenis_makanan({{ $num }})" class="btn btn-light"
-                                            type="button"
-                                            style="width: 100%;border: 1px solid black; border-radius: 10px;font-size: 10px;">{{ $f->nama_makanan }}</button>
-                                    </div>
+                                @if ($num <= 3)
+                                <div class="col-lg-3 mb-3">
+                                    <input id="input_jenis_makanan{{ $num }}" type="hidden"
+                                        value="{{ $f->nama_makanan }}">
+                                    <button id="pilih_jenis_makanan{{ $num }}"
+                                        onclick="pilih_jenis_makanan({{ $num }})" class="btn btn-light"
+                                        type="button"
+                                        style="width: 100%;border: 1px solid black; border-radius: 10px;font-size: 10px;">{{ $f->nama_makanan }}</button>
+                                </div>
+                                @else
+                                <div class="collapse" id="colapseExample{{$num}}">
+                                <div class="col-lg-3 mb-3" style="margin-left: -10px;">
+                                    <input id="input_jenis_makanan{{ $num }}" type="hidden"
+                                        value="{{ $f->nama_makanan }}">
+                                    <button id="pilih_jenis_makanan{{ $num }}"
+                                        onclick="pilih_jenis_makanan({{ $num }})" class="btn btn-light"
+                                        type="button"
+                                        style="width: 100%;border: 1px solid black; border-radius: 10px;font-size: 10px;">{{ $f->nama_makanan }}</button>
+                                </div>
+                                </div>
+                                <a class="text-center text-dark" data-bs-toggle="collapse" href="#colapseExample{{$num}}" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                    Lihat Lebih Banyak <i class="fa-solid fa-chevron-down"></i>
+                                  </a>    
+                                @endif
                                 @endforeach
                             </div>
                         </div>
