ce77e1041c90d5d4c994d5a8d54edff26a453c98
frdynsyhz <138851667+frdynsyhz@users.noreply.github.com>
2023-09-11 12:57:10 +0700

    update ui

--- resources/views/template/nav.blade.php
+++ resources/views/template/nav.blade.php
@@ -729,8 +729,14 @@
                                                         <path fill="currentColor"
                                                             d="M19 3H5v18l7-3l7 3V3zm-2 15l-5-2.18L7 18V5h10v13z" />
                                                     </svg>
-                                                    favorite
+                                                    Favorite
                                                 </a>
+                                                <div class="dropdown-divider"></div>
+                                               <a href="#"  style="width: 230px;"
+                                                    class="dropdown-item text-orange">
+                                                    <svg class="me-2" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M14.005 2.003a8 8 0 0 1 3.292 15.293A8 8 0 1 1 6.711 6.71a8.003 8.003 0 0 1 7.294-4.707Zm-4 6a6 6 0 1 0 0 12a6 6 0 0 0 0-12Zm1 1v1h2v2h-4a.5.5 0 0 0-.09.992l.09.008h2a2.5 2.5 0 0 1 0 5v1h-2v-1h-2v-2h4a.5.5 0 0 0 .09-.992l-.09-.008h-2a2.5 2.5 0 0 1 0-5v-1h2Zm3-5A5.985 5.985 0 0 0 9.52 6.016a8 8 0 0 1 8.47 8.471a6 6 0 0 0-3.986-10.484Z"/></svg>
+                                                    Upgrade
+                                                    </a>
                                                 <div class="dropdown-divider"></div>
                                                 <a href="{{ route('actionlogout') }}" style="width: 230px;"
                                                     class="dropdown-item text-orange">
@@ -743,7 +749,6 @@
                                                     </svg>
                                                     Keluar
                                                 </a>
-                                                <div class="dropdown-divider"></div>
                                             </div>
                                     </div>
                                 @elseif (auth()->user()->role === 'admin')
