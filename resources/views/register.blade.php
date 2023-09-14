a0128dafd62862c8d5a0bcb8f4a142783dd02052
Apis.nr <122329006+apis098@users.noreply.github.com>
2023-09-14 20:04:17 +0700

    fig bug image from apis

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -331,7 +331,7 @@ border-radius: 10px;
                                 <path fill="currentColor"
                                     d="M5 21q-.825 0-1.413-.588T3 19v-6h2v6h6v2H5Zm8 0v-2h6v-6h2v6q0 .825-.588 1.413T19 21h-6Zm-7-4l3-4l2.25 3l3-4L18 17H6Zm-3-6V5q0-.825.588-1.413T5 3h6v2H5v6H3Zm16 0V5h-6V3h6q.825 0 1.413.588T21 5v6h-2Zm-3.5-1q-.65 0-1.075-.425T14 8.5q0-.65.425-1.075T15.5 7q.65 0 1.075.425T17 8.5q0 .65-.425 1.075T15.5 10Z" />
                             </svg>
-                                    <img src="" style="display: none; margin-left: -7px" width="115" height="100"
+                                    <img src="" style="display: none; margin-left: -13px" width="115" height="100"
                                         alt="" class="" id="profile-image">
                                 </div>
                             </div>
