9f01d28d7e9374cc016dcc721000c2c2700a4751
Apis.nr <122329006+apis098@users.noreply.github.com>
2023-09-15 09:06:48 +0700

    update from apis
    update apis

--- resources/views/vendor/Chatify/layouts/messageCard.blade.php
+++ resources/views/vendor/Chatify/layouts/messageCard.blade.php
@@ -13,24 +13,27 @@ $timeAndSeen = "<span data-time='$created_at' class='message-time'>
         </div>
     @endif
     {{-- Card --}}
-    <div class="message-card-content">
+    <div class="message-card-content" style="background-color:{{$isSender ? "#F7941E":"rgba(255, 255, 255, 0.67"}}">
         @if (@$attachment->type != 'image' || $message)
             <div class="message">
                 {!! ($message == null && $attachment != null && @$attachment->type != 'file') ? $attachment->title : nl2br($message) !!}
                 {!! $timeAndSeen !!}
                 {{-- If attachment is a file --}}
                 @if(@$attachment->type == 'file')
+                <video style="margin-top:-5%;" controls width="360" height="250">
+                    <source src="/storage/attachments/{{$attachment->file}}" type="video/mp4">
+                  </video>
                 <a href="{{ route(config('chatify.attachments.download_route_name'), ['fileName'=>$attachment->file]) }}" class="file-download">
                     <span class="fas fa-file"></span> {{$attachment->title}}</a>
                 @endif
             </div>
         @endif
         @if(@$attachment->type == 'image')
-        <div class="image-wrapper" style="text-align: {{$isSender ? 'end' : 'start'}}">
-            <div class="image-file chat-image" style="background-image: url('/storage/attachments/{{$attachment->file}}')">
+        <div class="image-wrapper" style="text-align: {{$isSender ? 'end' : 'start'}}; ">
+            <div class="image-file chat-image" style="background-image: url('/storage/attachments/{{$attachment->file}}');">
                 <div>{{ $attachment->title }}</div>
             </div>
-            <div style="margin-bottom:5px" class="ms-3 mt-1">
+            <div style="margin-bottom:5px" class=" @if(!$isSender) ms-3 @else me-3 text-light @endif mt-1">
                 {!! $timeAndSeen !!}
             </div>
         </div>
