7c48eba09c82324c0621c8da44a336c4873b4b0f
Ghofur102 <muhamadabdulghofur84@gmail.com>
2023-09-24 18:39:29 +0700

    responsive resep.blade.php

--- resources/views/template/resep.blade.php
+++ resources/views/template/resep.blade.php
@@ -100,16 +100,16 @@
             }
 
             /* button{
-                                                                                                                            background-color: #F7941E;
-                                                                                                                            border: none;
-                                                                                                                            height: 45px;
-                                                                                                                            width: 90px;
-                                                                                                                            color: #ffffff;
-                                                                                                                            position: absolute;
-                                                                                                                            right: 1px;
-                                                                                                                            top: 0px;
-                                                                                                                            border-radius: 15px
-                                                                                                                        } */
+                                                                                                                                background-color: #F7941E;
+                                                                                                                                border: none;
+                                                                                                                                height: 45px;
+                                                                                                                                width: 90px;
+                                                                                                                                color: #ffffff;
+                                                                                                                                position: absolute;
+                                                                                                                                right: 1px;
+                                                                                                                                top: 0px;
+                                                                                                                                border-radius: 15px
+                                                                                                                            } */
             .search-2 i {
                 position: absolute;
                 top: 12px;
@@ -221,7 +221,14 @@
                                 <path fill="currentColor"
                                     d="M15 19.88c.04.3-.06.62-.29.83a.996.996 0 0 1-1.41 0L9.29 16.7a.989.989 0 0 1-.29-.83v-5.12L4.21 4.62a1 1 0 0 1 .17-1.4c.19-.14.4-.22.62-.22h14c.22 0 .43.08.62.22a1 1 0 0 1 .17 1.4L15 10.75v9.13M7.04 5L11 10.06v5.52l2 2v-7.53L16.96 5H7.04Z" />
                             </svg>
-                            filter
+                            <span id="filter">filter</span>
+                            <style>
+                                @media only screen and (max-width: 1200px) {
+                                    #filter {
+                                        display: none;
+                                    }
+                                }
+                            </style>
                         </button>
                     </div>
                 </div>
@@ -457,8 +464,9 @@
             @foreach ($recipes as $num => $item)
                 <div class="col-lg-4 mb-2 col-sm-12 col-md-6">
                     <div class="card" style="border-radius: 15px; border: 0.50px black solid">
-                        <div class="card-header my-3 mx-auto" style="background-color: white">
-                            <img width="260px" class="rounded-circle" height="260px"
+                        <div class="card-header my-3 text-center mx-auto" style="background-color: white;width:100%;">
+                            
+                            <img width="80%" class="rounded-circle" height="80%"
                                 style="border: 0.50px black solid; max-width:260px;"
                                 src="{{ asset('storage/' . $item->foto_resep) }}" />
                             @if ($item->isPremium === 'yes')
