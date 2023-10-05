05a01c6fcecf4f04f80f354474d34ee0cb61c5b8
frdynsyhz <138851667+frdynsyhz@users.noreply.github.com>
2023-10-05 10:51:19 +0700

    Menambahkan Profil pada halaman koki

--- app/Http/Controllers/KokiController.php
+++ app/Http/Controllers/KokiController.php
@@ -85,7 +85,26 @@ class KokiController extends Controller
 
     public function profilage(Request $request)
     {
-        return view('koki.profilage');
+        $userLogin = Auth::user();
+        $notification = [];
+        $favorite = [];
+        $unreadNotificationCount=[];
+        $messageCount = [];
+        if ($userLogin) {
+            $messageCount = ChMessage::where('to_id', auth()->user()->id)->where('seen', '0')->count();
+        }
+        if ($userLogin) {
+            $notification = notifications::where('user_id', auth()->user()->id)
+                ->orderBy('created_at', 'desc') // Urutkan notifikasi berdasarkan created_at terbaru
+                ->paginate(10); // Paginasi notifikasi dengan 10 item per halaman
+                $unreadNotificationCount = notifications::where('user_id',auth()->user()->id)->where('status', 'belum')->count();
+        }
+        if ($userLogin) {
+            $favorite = favorite::where('user_id_from', auth()->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+        }
+        return view('koki.profilage', compact('messageCount','notification','unreadNotificationCount','userLogin','favorite'));
     }
 
     public function feed(Request $request)
