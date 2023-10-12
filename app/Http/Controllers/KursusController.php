33b0757f92438f4efb4a0a820026c6377ae6bf09
Ghofur102 <muhamadabdulghofur84@gmail.com>
2023-10-12 08:06:06 +0700

    memperbaiki bug kursus dashbord koki

--- app/Http/Controllers/KursusController.php
+++ app/Http/Controllers/KursusController.php
@@ -143,7 +143,27 @@ class KursusController extends Controller
      */
     public function create()
     {
-       //
+        $userLogin = Auth::user();
+        $notification = [];
+        $favorite = [];
+        $footer = footer::first();
+        $unreadNotificationCount = [];
+        $messageCount = [];
+        if ($userLogin) {
+            $messageCount = ChMessage::where('to_id', auth()->user()->id)->where('seen', '0')->count();
+        }
+        if ($userLogin) {
+            $notification = notifications::where('user_id', auth()->user()->id)
+                ->orderBy('created_at', 'desc') // Urutkan notifikasi berdasarkan created_at terbaru
+                ->paginate(10); // Paginasi notifikasi dengan 10 item per halaman
+            $unreadNotificationCount = notifications::where('user_id', auth()->user()->id)->where('status', 'belum')->count();
+        }
+        if ($userLogin) {
+            $favorite = favorite::where('user_id_from', auth()->user()->id)
+                ->orderBy('created_at', 'desc')
+                ->paginate(10);
+        }
+        return view('kursus.index', compact('messageCount', 'notification', 'unreadNotificationCount', 'userLogin', 'footer', 'favorite'));
     }
 
     /**
