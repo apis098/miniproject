95afa9f08e816166e481de7fb8e0f1b7f63280ff
frdynsyhz <138851667+frdynsyhz@users.noreply.github.com>
2023-10-17 11:57:55 +0700

    up

--- resources/views/koki/feed.blade.php
+++ resources/views/koki/feed.blade.php
@@ -776,7 +776,7 @@
                                         </a>
                                         <div style="margin-top: 8px;">
                                             <a href="" class="text-dark ">
-                                                <strong class="text-center ">Sikukuntul</strong>
+                                                <strong class="text-center ">Sikukuntu</strong>
                                             </a>
                                             <a href="#" class="text-muted d-block"
                                                 style="float: right; margin-left: 390px;">
@@ -825,8 +825,7 @@
                                                 </svg>
                                             </a>
                                                     <strong class="text-dark me-2">Siapapun Itu Kocak</strong>
-                                                    <br>
-                                                     <span>Bagus Banget bbdkadajddj bddadakdjasd bkdadkahad bdadhadad adbdhakda ddkdkda dadhd ddkhd dhdkhda k!!!</span>
+                                                     <span>Bagus Banget masakan apa ini sepertinya sangat enak untuk dikonsumsi dan sangat menyehatan untuk badan kita teman temanku ya gaesya semua marilah kita emot epep!!!</span>
 
 
                                                 <div class="d-flex">
@@ -843,9 +842,19 @@
                                         </a>
                                             <div class="col-11">
                                             <div class=" rounded-3 py-1">
-                                                <a href="" class="text-dark mb-0">
-                                                    <strong>Siapapun Itu Kocak</strong>
-                                                </a>
+                                                <strong class="text-dark me-2">Siapapun Itu Kocak</strong>
+                                                <br>
+                                                <a type="button" class="mr-3 text-dark float-end" onclick="openModel()" id="button-modal-komentar-feed"
+                                                href="#" data-bs-toggle="modal"data-bs-target="#exampleModal">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512">
+                                                    <path fill="currentColor"
+                                                        d="M323.8 34.8c-38.2-10.9-78.1 11.2-89 49.4l-5.7 20c-3.7 13-10.4 25-19.5 35l-51.3 56.4c-8.9 9.8-8.2 25 1.6 33.9s25 8.2 33.9-1.6l51.3-56.4c14.1-15.5 24.4-34 30.1-54.1l5.7-20c3.6-12.7 16.9-20.1 29.7-16.5s20.1 16.9 16.5 29.7l-5.7 20c-5.7 19.9-14.7 38.7-26.6 55.5c-5.2 7.3-5.8 16.9-1.7 24.9s12.3 13 21.3 13H448c8.8 0 16 7.2 16 16c0 6.8-4.3 12.7-10.4 15c-7.4 2.8-13 9-14.9 16.7s.1 15.8 5.3 21.7c2.5 2.8 4 6.5 4 10.6c0 7.8-5.6 14.3-13 15.7c-8.2 1.6-15.1 7.3-18 15.1s-1.6 16.7 3.6 23.3c2.1 2.7 3.4 6.1 3.4 9.9c0 6.7-4.2 12.6-10.2 14.9c-11.5 4.5-17.7 16.9-14.4 28.8c.4 1.3.6 2.8.6 4.3c0 8.8-7.2 16-16 16h-97.5c-12.6 0-25-3.7-35.5-10.7l-61.7-41.1c-11-7.4-25.9-4.4-33.3 6.7s-4.4 25.9 6.7 33.3l61.7 41.1c18.4 12.3 40 18.8 62.1 18.8H384c34.7 0 62.9-27.6 64-62c14.6-11.7 24-29.7 24-50c0-4.5-.5-8.8-1.3-13c15.4-11.7 25.3-30.2 25.3-51c0-6.5-1-12.8-2.8-18.7c11.6-11.7 18.8-27.7 18.8-45.4c0-35.3-28.6-64-64-64h-92.3c4.7-10.4 8.7-21.2 11.8-32.2l5.7-20c10.9-38.2-11.2-78.1-49.4-89zM32 192c-17.7 0-32 14.3-32 32v224c0 17.7 14.3 32 32 32h64c17.7 0 32-14.3 32-32V224c0-17.7-14.3-32-32-32H32z" />
+                                                </svg>
+                                            </a>
+                                            <div style="max-width: 90%;">
+                                                     <span >Bagus Banget masakan apa ini sepertinya sangat enak untuk dikonsumsi dan sangat menyehatan untuk badan kita teman temanku  ya gaesya semua marilah kita emot epep!!!!!!</span>
+                                                    </div>
+
                                                 <div class="d-flex">
                                                     <small class="text-muted d-block me-3">1 hari yang lalu</small>
                                                     <small class="text-bold">Balas</small>
