482fae5aa7bbd6704ecc310bc01190b666988445
frdynsyhz <138851667+frdynsyhz@users.noreply.github.com>
2023-10-22 20:06:50 +0700

    update tawaran admin

--- resources/views/admin/tawaran.blade.php
+++ resources/views/admin/tawaran.blade.php
@@ -181,7 +181,8 @@
             }
         }
     </style>
-    <div class="d-flex mt-3 ms-5  justify-content-start">
+    <div style="overflow-x: hidden">
+    <div class="d-flex mt-3 ms-5  justify-content-start" >
         <ul class="nav mb-3 mr-5" id="pills-tab" role="tablist">
             <li class="nav-item tabs" role="presentation">
                 <a href="#" class="nav-link mr-5 active" id="button-berlangganan" data-bs-toggle="tab"
@@ -447,6 +448,22 @@
                             <div class="col-lg-4 col-md-6">
                                 <div class="pricing-item animated-card">
                                     <div class="pricing-heading">
+                                        <button type="submit "
+                                        style=" right: 10%; top: -2%; position: absolute; background-color:#F7941E; border: none; width: 10%; height: 7%;"
+                                        class="btn btn-warning btn-sm text-light rounded-circle p-2" data-bs-toggle="modal"
+                                        data-bs-target="#mymodal">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                                viewBox="0 0 24 24">
+                                                <path fill="none" stroke="currentColor" stroke-width="1.5"
+                                                    d="m14.36 4.079l.927-.927a3.932 3.932 0 0 1 5.561 5.561l-.927.927m-5.56-5.561s.115 1.97 1.853 3.707C17.952 9.524 19.92 9.64 19.92 9.64m-5.56-5.561l-8.522 8.52c-.577.578-.866.867-1.114 1.185a6.556 6.556 0 0 0-.749 1.211c-.173.364-.302.752-.56 1.526l-1.094 3.281m17.6-10.162L11.4 18.16c-.577.577-.866.866-1.184 1.114a6.554 6.554 0 0 1-1.211.749c-.364.173-.751.302-1.526.56l-3.281 1.094m0 0l-.802.268a1.06 1.06 0 0 1-1.342-1.342l.268-.802m1.876 1.876l-1.876-1.876" />
+                                            </svg>
+                                    </button>
+                                        <button type="button "
+                                        style=" right: -4%; top: -3%; position: absolute;"
+                                        class="btn  btn-sm text-light rounded-circle p-2" data-bs-toggle="modal"
+                                        data-bs-target="#mymodal">
+                                        <i class="fa-regular text-danger fa-circle-xmark fa-2xl" ></i>
+                                    </button>
                                         <!-- Title -->
                                         <div class="title change-color">
                                             <h6>{{ $item_prem->nama_paket }}</h6>
@@ -601,6 +618,7 @@
             </div>
         </div>
     </div>
+</div>
     <script>
         const btnTopUp = document.getElementById("button-topUp");
         const btnBerlangganan = document.getElementById("button-berlangganan");
