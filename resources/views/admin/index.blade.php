5fb561447467fee5552428dd1964fcef01a8fa56
mhadinurrokhim <muhamadabdulghofur84@gmail.com>
2023-12-06 04:28:21 +0700

    melanjutkan mengerjakan dashboard admin dan mengerjakan responsive nav admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,7 +4,7 @@
       <div class="mx-auto my-3">
         <div class="row">
           <div class="col-md-4 col-sm-12">
-            <div class="card p-3"style="border: 1px solid #333;border-radius:15px;">
+            <div class="card px-3 py-4"style="border: 1px solid #333;border-radius:15px;">
               <div class="d-flex justify-content-between">
                 <div class="">
                   <h6 class="mb-0" style="font-size: 24px; font-weight: bold;">{{ $jumlah_user }}</h6>
@@ -17,11 +17,11 @@
             </div>
           </div>
           <div class="col-md-4 col-sm-12">
-            <div class="card p-3"style="border: 1px solid #333;border-radius:15px;">
+            <div class="card px-3 py-4"style="border: 1px solid #333;border-radius:15px;">
               <div class="d-flex justify-content-between">
                 <div class="">
-                  <h6 class="mb-0" style="font-size: 24px; font-weight: bold;">{{ $jumlah_user }}</h6>
-                  <p class="mb-2" style="font-size: 14px; font-weight: bold;">Pengguna</p>
+                  <h6 class="mb-0" style="font-size: 24px; font-weight: bold;">{{ $jumlah_report }}</h6>
+                  <p class="mb-2" style="font-size: 14px; font-weight: bold;">Laporan</p>
                 </div>
                 <div class="">
                   <i class="fas fa-user-circle fa-3x"></i>
@@ -30,11 +30,11 @@
             </div>
           </div>
           <div class="col-md-4 col-sm-12">
-            <div class="card p-3"style="border: 1px solid #333;border-radius:15px;">
+            <div class="card px-3 py-4"style="border: 1px solid #333;border-radius:15px;">
               <div class="d-flex justify-content-between">
                 <div class="">
-                  <h6 class="mb-0" style="font-size: 24px; font-weight: bold;">{{ $jumlah_user }}</h6>
-                  <p class="mb-2" style="font-size: 14px; font-weight: bold;">Pengguna</p>
+                  <h6 class="mb-0" style="font-size: 24px; font-weight: bold;">{{ $jumlah_resep }}</h6>
+                  <p class="mb-2" style="font-size: 14px; font-weight: bold;">Resep</p>
                 </div>
                 <div class="">
                   <i class="fas fa-user-circle fa-3x"></i>
@@ -45,21 +45,22 @@
         </div>
         <div class="d-flex justify-content-between">
           <div class="">
-            <select name="" id="" class="form-select"
-              style="background-color: #F7941E;border-radius:15px;border: none;color: white;box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);">
-              <option style="background-color:white;color:black;" value="">
+            <div class="dropdown">
+              <button class="btn dropdown-toggle"
+                style="background-color: #F7941E;border-radius:15px;border: none;color: white;box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);"
+                type="button" data-bs-toggle="dropdown" aria-expanded="false">
                 @if (request()->has('tahun'))
                   {{ request()->tahun }}
                 @else
                   {{ date('Y') }}
                 @endif
-              </option>
-              @foreach ($years as $y)
-                <option value="" style="background-color:white;color:black;">
-                  <a class="dropdown-item" href="/admin/dashboard?tahun={{ $y }}">{{ $y }}</a>
-                </option>
-              @endforeach
-            </select>
+              </button>
+              <ul class="dropdown-menu" style="height: 100px;overflow:scroll;">
+                @foreach ($years as $y)
+                  <li><a class="dropdown-item" href="/admin/dashboard?tahun={{ $y }}">{{ $y }}</a></li>
+                @endforeach
+              </ul>
+            </div>
           </div>
           <div class="">
             <h5 class="fw-bold">Data Jumlah Pengguna</h5>
@@ -70,38 +71,38 @@
           <div class="col-sm-12 col-md-6">
             <h5 class="fw-bold">Resep baru dari koki</h5> <br>
             <div class="card p-3" style="border:1px solid #333;border-radius:15px;height:400px;">
-            @foreach ($reseps as $rsp)
-              <div class="border-bottom py-3">
-                <a href="#" class="text-decoration-none d-flex text-dark">
-                  <img class="rounded-circle flex-shrink-0" src="{{ asset('storage/' . $rsp->foto_resep) }}"
-                    alt="" style="width: 40px; height: 40px;">
-                  <div class="w-100 ms-3">
-                    <div class="d-flex w-100 justify-content-between">
-                      <h6 class="mb-0">{{ $rsp->nama_resep }}</h6>
-                      <small>{{ \Carbon\Carbon::parse($rsp->created_at)->locale('id_ID')->diffForHumans() }}</small>
+              @foreach ($reseps as $rsp)
+                <div class="border-bottom py-3">
+                  <a href="#" class="text-decoration-none d-flex text-dark">
+                    <img class="rounded-circle flex-shrink-0" src="{{ asset('storage/' . $rsp->foto_resep) }}"
+                      alt="" style="width: 40px; height: 40px;">
+                    <div class="w-100 ms-3">
+                      <div class="d-flex w-100 justify-content-between">
+                        <h6 class="mb-0">{{ $rsp->nama_resep }}</h6>
+                        <small>{{ \Carbon\Carbon::parse($rsp->created_at)->locale('id_ID')->diffForHumans() }}</small>
 
+                      </div>
+                      <span>
+                        Oleh {{ $rsp->user->name }}
+                      </span>
                     </div>
-                    <span>
-                      Oleh {{ $rsp->user->name }}
-                    </span>
-                  </div>
-                </a>
-              </div>
-            @endforeach
-            @forelse ($reseps as $rsp)
-            @empty
+                  </a>
+                </div>
+              @endforeach
+              @forelse ($reseps as $rsp)
+              @empty
 
-              <div class="d-flex flex-column h-100 justify-content-center align-items-center" style="margin-top: 2em">
-                <img src="{{ asset('images/data.png') }}" style="width: 15em">
-                <p style="color: #1d1919"><b>Tidak ada data</b></p>
-              </div>
-            @endforelse
+                <div class="d-flex flex-column h-100 justify-content-center align-items-center" style="margin-top: 2em">
+                  <img src="{{ asset('images/data.png') }}" style="width: 15em">
+                  <p style="color: #1d1919"><b>Tidak ada data</b></p>
+                </div>
+              @endforelse
             </div>
           </div>
           <div class="col-sm-12 col-md-6">
             <h5 class="fw-bold">Laporan pengguna</h5> <br>
             <div class="card p-3" style="border: 1px solid #333;border-radius:15px;height:400px;">
-                @foreach ($reports as $r)
+              @foreach ($reports as $r)
                 <!-- Konten laporan terbaru -->
                 <div class="border-bottom py-3">
                   <a href="#" class="text-decoration-none d-flex text-dark">
@@ -124,8 +125,7 @@
               @endforeach
               @forelse ($reports as $r)
               @empty
-                <div class="d-flex flex-column h-100 justify-content-center align-items-center"
-                  style="margin-top: 2em">
+                <div class="d-flex flex-column h-100 justify-content-center align-items-center" style="margin-top: 2em">
                   <img src="{{ asset('images/data.png') }}" style="width: 15em">
                   <p style="color: #1d1919"><b>Tidak ada data</b></p>
                 </div>
