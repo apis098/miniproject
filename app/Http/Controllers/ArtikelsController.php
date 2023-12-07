39937f866a1293e8b52381431d596fe2a03d8153
Apis.nr <122329006+apis098@users.noreply.github.com>
2023-12-07 10:36:15 +0700

    artikel

--- app/Http/Controllers/ArtikelsController.php
+++ app/Http/Controllers/ArtikelsController.php
@@ -3,19 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Events\ShowRecipePremium;
-use App\Models\basic_tips;
 use App\Models\ChMessage;
-use App\Models\comment_recipes;
-use App\Models\favorite;
-use App\Models\reseps;
-use App\Models\seputar_dapur;
-use App\Models\special_days;
+use App\Models\CommentResipes;
+use App\Models\Favorite;
+use App\Models\Reseps;
+use App\Models\SpecialDays;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\notifications;
-use App\Models\footer;
-use App\Models\income_chefs;
-use App\Models\like_comment_recipes;
+use App\Models\Notifications;
+use App\Models\Footer;
+use App\Models\IncomeChefs;
+use App\Models\LikeCommentRecips;
 use App\Models\ResepPremiums;
 use App\Models\Share;
 use App\Models\TopUpCategories;
@@ -27,8 +25,8 @@ class ArtikelsController extends Controller
     public function artikel_resep(string $id, string $judul)
     {
         // check isPremium
-        $show_resep = reseps::find($id);
-        $r = reseps::find($id);
+        $show_resep = Reseps::find($id);
+        $r = Reseps::find($id);
         $isPremium = $r->isPremium;
         if ($isPremium == "yes") {
             event(new ShowRecipePremium(Auth::user()->id, $r->user->id, $id, "resep"));
@@ -57,23 +55,23 @@ class ArtikelsController extends Controller
             if ($id_user == $id_admin->id) {
                 $admin = true;
             }
-            $notification = notifications::where('user_id', auth()->user()->id)
+            $notification = Notifications::where('user_id', auth()->user()->id)
             ->where('status','belum')
             ->orderBy('created_at', 'desc')
             ->paginate(10);
-            $unreadNotificationCount = notifications::where('user_id', auth()->user()->id)->where('status', 'belum')->count();
+            $unreadNotificationCount = Notifications::where('user_id', auth()->user()->id)->where('status', 'belum')->count();
             // jika user sudah login
             $userLog = 2;
-            $gift_check = income_chefs::where('user_id',auth()->user()->id)->where('resep_id',$show_resep->id)->whereNot('status','sawer')->count();
-            $gift_count = income_chefs::where('resep_id',$show_resep->id)->where('status','sawer')->count();
+            $gift_check = IncomeChefs::where('user_id',auth()->user()->id)->where('resep_id',$show_resep->id)->whereNot('status','sawer')->count();
+            $gift_count = IncomeChefs::where('resep_id',$show_resep->id)->where('status','sawer')->count();
             $share_check = Share::where('sender_id',auth()->user()->id)->where('resep_id',$show_resep->id)->count();
         }
         if ($userLogin) {
-            $favorite = favorite::where('user_id_from', auth()->user()->id)
+            $favorite = Favorite::where('user_id_from', auth()->user()->id)
                 ->orderBy('created_at', 'desc')
                 ->paginate(10);
         }
-        $footer = footer::first();
+        $footer = Footer::first();
         $comment = $show_resep->comment_recipes->sortByDesc('likes');
         $comment_count = $comment->count();
         $categorytopup  =  TopUpCategories::all();
