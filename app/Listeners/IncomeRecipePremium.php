8244af837033d90c03d3ae1df9d50944f32186b1
Apis.nr <122329006+apis098@users.noreply.github.com>
2023-12-07 10:40:01 +0700

    fix bug artikel remiums

--- app/Listeners/IncomeRecipePremium.php
+++ app/Listeners/IncomeRecipePremium.php
@@ -4,9 +4,9 @@ namespace App\Listeners;
 
 use App\Events\ShowRecipePremium;
 use App\Models\User;
-use App\Models\income_chefs;
-use App\Models\reseps;
-use App\Models\upload_video;
+use App\Models\IncomeChefs;
+use App\Models\Reseps;
+use App\Models\UploadVideo;
 use App\Models\Share;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
@@ -34,7 +34,7 @@ class IncomeRecipePremium
                $level_koki = $kokis->level_koki / 10;
                $pemasukan = 100 * $level_koki;
            }
-           $income = income_chefs::where('chef_id', $event->chef)
+           $income = IncomeChefs::where('chef_id', $event->chef)
            ->where('user_id', $event->user)
            ->where('resep_id', $event->content)
            ->where('status', $event->status)
@@ -42,7 +42,7 @@ class IncomeRecipePremium
            $pengguna = User::find($event->user);
            if ($event->chef != $event->user && $pengguna->role != "admin") {
                if ($income != true) {
-                   income_chefs::create([
+                   IncomeChefs::create([
                        "chef_id" => $event->chef,
                        "user_id" => $event->user,
                        "resep_id" => $event->content,
@@ -59,21 +59,21 @@ class IncomeRecipePremium
                  // memperbarui level koki
                 // menghitung total like, share, view, favorite, dan followers
                 // menghitung total popularitas seluruhnya
-                $total_like_feed_semua = upload_video::whereHas("like_veed")->count();
-                $total_like_resep_semua = reseps::whereHas("likes")->count();
+                $total_like_feed_semua = UploadVideo::whereHas("like_veed")->count();
+                $total_like_resep_semua = Reseps::whereHas("likes")->count();
                 $total_share_semua = Share::count();
-                $total_view_semua = income_chefs::count();
-                $total_favorite_feed_semua = upload_video::whereHas("favorite")->count();
-                $total_favorite_resep_semua = reseps::whereHas("favorite")->count();
+                $total_view_semua = IncomeChefs::count();
+                $total_favorite_feed_semua = UploadVideo::whereHas("favorite")->count();
+                $total_favorite_resep_semua = Reseps::whereHas("favorite")->count();
                 $total_followers_semua = User::sum('followers');
                 $total_popularitas = $total_like_feed_semua + $total_like_resep_semua + $total_share_semua + $total_view_semua + $total_favorite_feed_semua + $total_favorite_resep_semua + $total_followers_semua;
                 // menghitung total popularitas chef
-                $total_like_feed = upload_video::where("users_id", $event->chef)->whereHas("like_veed")->count();
-                $total_like_resep = reseps::where("user_id", $event->chef)->whereHas("likes")->count();
+                $total_like_feed = UploadVideo::where("users_id", $event->chef)->whereHas("like_veed")->count();
+                $total_like_resep = Reseps::where("user_id", $event->chef)->whereHas("likes")->count();
                 $total_share = Share::where('user_id', $event->chef)->count();
-                $total_view = income_chefs::where("chef_id", $event->chef)->count();
-                $total_favorite_feed = upload_video::where("users_id", $event->chef)->whereHas("favorite")->count();
-                $total_favorite_resep = reseps::where("user_id")->whereHas("favorite")->count();
+                $total_view = IncomeChefs::where("chef_id", $event->chef)->count();
+                $total_favorite_feed = UploadVideo::where("users_id", $event->chef)->whereHas("favorite")->count();
+                $total_favorite_resep = Reseps::where("user_id")->whereHas("favorite")->count();
                 $total_followers = $koki->followers;
                 $total_popularitas_chef = $total_like_feed + $total_like_resep + $total_share + $total_view + $total_favorite_feed + $total_favorite_resep + $total_followers;
                 $level = $total_popularitas_chef / $total_popularitas;
