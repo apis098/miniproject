e203a23c729d4a46b7dca19ca61c7244c0491942
Apis.nr <122329006+apis098@users.noreply.github.com>
2023-12-11 14:52:59 +0700

    mengganti nama model yaang menimbulkan bug

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -9,7 +9,7 @@ use App\Models\Reply;
 use App\Models\ReplyComplaint;
 use App\Models\CommentResipes;
 use App\Models\ReplyCommentRecipe;
-use App\Models\CommentRecipes;
+use App\Models\CommentResipes;
 use App\Models\Report;
 use App\Models\Reseps;
 use App\Models\TopUpCategories;
@@ -520,9 +520,9 @@ class ReportController extends Controller
         return redirect('/resep')->with('success', 'Resep telah diblokir');
     }
     public function block_comment_recipe(Request $request,$id){
-        $comment = CommentRecipes::findOrFail($id);
+        $comment = CommentResipes::findOrFail($id);
         
-    }   
+    }
     public function block_complaint(Request $request, $id) {
         $complaint = Complaint::findOrFail($id);
         $complaint->user->increment('jumlah_pelanggaran');
