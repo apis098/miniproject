b59df5d76a35d4140dfb3033c277cdc4b7517897
Ghofur102 <137810153+Ghofur102@users.noreply.github.com>
2023-12-14 10:05:38 +0700

    Update kursus.blade.php

--- resources/views/admin/kursus.blade.php
+++ resources/views/admin/kursus.blade.php
@@ -470,7 +470,7 @@
                             <h5 class="modal-title fw-bolder">Kirim alasan
                             </h5>
                             <button type="button" class="btn-close"
-                                data-dismiss="modal"
+                                data-bs-dismiss="modal"
                                 aria-label="Close"></button>
                         </div>
                         <div class="modal-body" style="text-align: right;">
